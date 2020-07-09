a82f71e868ffef92170663e4183d3f0000dd1d72
Jasmine Carter <jasmine.carter87@gmail.com>
2020-07-09 01:55:50 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,7 +6,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    binding.pry
+    #binding.pry
     @landmark = Landmark.find_or_create_by(name: params["landmark"]["name"])
     erb :'/landmark/:id'
   end
