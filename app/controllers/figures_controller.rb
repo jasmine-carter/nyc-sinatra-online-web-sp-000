f44387fa925a824398abaf9da686811b02b025a6
Jasmine Carter <jasmine.carter87@gmail.com>
2020-07-09 00:58:41 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,6 +33,7 @@ class FiguresController < ApplicationController
   end
 
   patch '/figures/:id' do
+    binding.pry
     @figure = Figure.find_by(id: [params][:id])
     redirect "/figures/:id"
   end
